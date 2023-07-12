8705de23b5396d166d5d2ab52fae34714327aed8
姚一鸣 <yaoyiming123@live.com>
2023-07-12 19:47:48 +0800

    tray notify icon

---
+++ Flint3/Helpers/NotifyIconHelper.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flint3.Helpers
+{
+    // https://github.com/castorix/WinUI3_NotifyIcon
+    public class NotifyIconTools
+    {
+        public const int WM_USER = 0x0400;
+        public const int WM_TRAYMOUSEMESSAGE = WM_USER + 1024;
+
+        [DllImport("Shell32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool Shell_NotifyIcon(NIM dwMessage, [In] NOTIFYICONDATA lpdata);
+
+        public static bool TrayMessage(IntPtr hWnd, string sMessage, IntPtr hIcon, IntPtr hBalloonIcon, NIM nMessage, NIIF dwInfoFlags, string sInfo, string sTitle, int nTimeOut)
+        {
+            NOTIFYICONDATA nid = new NOTIFYICONDATA();
+
+            //nid.cbSize = 956;
+
+            /*if (IsOsVistaOrLater())
+            nid.cbSize = sizeof(NOTIFYICONDATA);
+            else
+            nid.cbSize = NOTIFYICONDATA_V3_SIZE;*/
+            nid.uID = 1;
+            nid.hWnd = hWnd;
+            nid.uFlags = NIF.MESSAGE | NIF.INFO;
+            nid.uCallbackMessage = WM_TRAYMOUSEMESSAGE;
+
+            if (sMessage != null && sMessage != "")
+            {
+                nid.szTip = sMessage;
+                nid.uFlags |= NIF.TIP;
+            }
+            if (hIcon != IntPtr.Zero)
+            {
+                nid.hIcon = hIcon;
+                nid.uFlags |= NIF.ICON;
+            }
+            if ((dwInfoFlags & NIIF.USER) != 0)
+            {
+                nid.hIcon = hIcon;
+                nid.hBalloonIcon = hBalloonIcon;
+                nid.uFlags |= NIF.ICON;
+            }
+            nid.dwInfoFlags = dwInfoFlags;
+            if (dwInfoFlags != 0 && sInfo != null && sTitle != null)
+            {
+                nid.szInfo = sInfo;
+                nid.szInfoTitle = sTitle;
+            }
+            else
+            {
+                nid.szInfo = null;
+                nid.szInfoTitle = null;
+            }
+            bool bRet;
+            nid.uVersion = 4;
+            if (nMessage == NIM.ADD)
+                Shell_NotifyIcon(NIM.SETVERSION, nid);
+            bRet = Shell_NotifyIcon(nMessage, nid);
+            return bRet;
+        }
+
+        public const int NIS_HIDDEN = 0x00000001;
+        public const int NIS_SHAREDICON = 0x00000002;
+    }
+
+    public enum NIM : uint
+    {
+        ADD = 0,
+        MODIFY = 1,
+        DELETE = 2,
+        SETFOCUS = 3,
+        SETVERSION = 4,
+    }
+
+    // From WPF/Winforms source
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public class NOTIFYICONDATA
+    {
+        public int cbSize = Marshal.SizeOf(typeof(NOTIFYICONDATA));
+        public IntPtr hWnd;
+        public int uID;
+        public NIF uFlags;
+        public int uCallbackMessage;
+        public IntPtr hIcon;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string szTip;
+        public int dwState = 0;
+        public int dwStateMask = 0;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string szInfo;
+        //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        //public char[] szInfo = new char[256];
+        // Prior to Vista this was a union of uTimeout and uVersion.  As of Vista, uTimeout has been deprecated.
+        public uint uVersion;  // Used with Shell_NotifyIcon flag NIM_SETVERSION. 
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+        public string szInfoTitle;
+        public NIIF dwInfoFlags;
+        public Guid guidItem;
+        // Vista only
+        public IntPtr hBalloonIcon;
+    }
+
+    [Flags]
+    public enum NIS : uint
+    {
+        HIDDEN = 0x00000001,
+        SHAREDICON = 0x00000002
+    }
+
+    [Flags]
+    public enum NIF : uint
+    {
+        MESSAGE = 0x0001,
+        ICON = 0x0002,
+        TIP = 0x0004,
+        STATE = 0x0008,
+        INFO = 0x0010,
+        GUID = 0x0020,
+        REALTIME = 0x0040,
+        SHOWTIP = 0x0080,
+        XP_MASK = MESSAGE | ICON | STATE | INFO | GUID,
+        VISTA_MASK = XP_MASK | REALTIME | SHOWTIP,
+    }
+
+    [Flags]
+    public enum NIIF : uint
+    {
+        NONE = 0x00000000,
+        // icon flags are mutually exclusive
+        // and take only the lowest 2 bits
+        INFO = 0x00000001,
+        WARNING = 0x00000002,
+        ERROR = 0x00000003,
+        USER = 0x00000004,
+        ICON_MASK = 0x0000000F,
+        NOSOUND = 0x00000010,
+        LARGE_ICON = 0x00000020,
+        RESPECT_QUIET_TIME = 0x00000080
+    }
+}
--- Flint3/MainWindow.xaml.cs
+++ Flint3/MainWindow.xaml.cs
@@ -21,6 +21,10 @@ using Windows.System;
 using Windows.UI.ViewManagement;
 using WinUIEx;
 using Flint3.Views;
+using Flint3.Helpers;
+
+using static Flint3.Helpers.NotifyIconTools;
+using ABI.Windows.Foundation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,10 +39,12 @@ namespace Flint3
         public MainWindow()
         {
             this.InitializeComponent();
+            this.SystemBackdrop = new Microsoft.UI.Xaml.Media.MicaBackdrop();
 
             AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/Logos/flint_logo.ico"));
             PersistenceId = "FlintMainWindow";
 
+            // 初始导航页面，注册后退快捷键
             MainFrame.Loaded += (s, e) =>
             {
                 MainFrame.Navigate(typeof(FlintPage));
@@ -48,7 +54,49 @@ namespace Flint3
                 MainFrame.KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.GoBack));
                 MainFrame.KeyboardAccelerators.Add(BuildKeyboardAccelerator(VirtualKey.XButton1));
             };
+
+            hWndMain = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            SubClassDelegate = new SUBCLASSPROC(WindowSubClass);
+            bool bRet = SetWindowSubclass(hWndMain, SubClassDelegate, 0, 0);
+
+            m_hIcon = LoadImage(IntPtr.Zero, @"Assets\Logos\flint_logo.ico", IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
+            m_hBalloonIcon = LoadImage(IntPtr.Zero, @"Assets\Logos\flint_logo.ico", IMAGE_ICON, 128, 128, LR_LOADFROMFILE);
+
+            // 创建常驻托盘图标
+            TrayMessage(hWndMain, "燧石", m_hIcon, IntPtr.Zero, NIM.ADD, NIIF.NONE, null, null, 0);
         }
+
+        private void OnMainWindowClosed(object sender, WindowEventArgs args)
+        {
+            CleanTray();
+        }
+
+        private void OnClickHide(object sender, RoutedEventArgs e)
+        {
+            HideInTray();
+        }
+
+        private void OnClickPin(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (PinToggleButton?.IsChecked == true)
+                {
+                    this.IsAlwaysOnTop = true;
+                }
+                else
+                {
+                    this.IsAlwaysOnTop = false;
+                }
+            }
+            catch
+            {
+                PinToggleButton.IsChecked = false;
+                this.IsAlwaysOnTop = false;
+            }
+        }
+
+        #region Go Back
 
         private KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null)
         {
@@ -80,5 +128,202 @@ namespace Flint3
             catch { }
             return false;
         }
+
+        #endregion
+
+        #region NotifyIcon
+
+        // 来源 https://github.com/castorix/WinUI3_NotifyIcon
+
+        public static int HIWORD(int n)
+        {
+            return (n >> 16) & 0xffff;
+        }
+        public static int LOWORD(int n)
+        {
+            return n & 0xffff;
+        }
+
+        public delegate int SUBCLASSPROC(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam, IntPtr uIdSubclass, uint dwRefData);
+
+        [DllImport("Comctl32.dll", SetLastError = true)]
+        public static extern bool SetWindowSubclass(IntPtr hWnd, SUBCLASSPROC pfnSubclass, uint uIdSubclass, uint dwRefData);
+
+        [DllImport("Comctl32.dll", SetLastError = true)]
+        public static extern int DefSubclassProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+
+        public const int WM_CONTEXTMENU = 0x007B;
+        public const int WM_LBUTTONDOWN = 0x0201;
+        public const int WM_LBUTTONUP = 0x0202;
+        public const int WM_RBUTTONDOWN = 0x0204;
+        public const int WM_RBUTTONUP = 0x0205;
+        public const int WM_ENTERMENULOOP = 0x0211;
+        public const int WM_EXITMENULOOP = 0x0212;
+        public const int WM_INITMENUPOPUP = 0x0117;
+        public const int WM_UNINITMENUPOPUP = 0x0125;
+
+        public const int WM_DRAWITEM = 0x002B;
+        public const int WM_MEASUREITEM = 0x002C;
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool GetCursorPos(out Windows.Graphics.PointInt32 lpPoint);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr CreatePopupMenu();
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern int GetMenuItemCount(IntPtr hMenu);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool DestroyMenu(IntPtr hMenu);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool AppendMenu(IntPtr hMenu, uint uFlags, IntPtr uIDNewItem, string lpNewItem);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool ModifyMenu(IntPtr hMnu, uint uPosition, uint uFlags, IntPtr uIDNewItem, IntPtr lpNewItem);
+
+        public const int MF_STRING = 0x00000000;
+        public const int MF_BITMAP = 0x00000004;
+        public const int MF_OWNERDRAW = 0x00000100;
+
+        public const int MF_POPUP = 0x00000010;
+        public const int MF_MENUBARBREAK = 0x00000020;
+        public const int MF_MENUBREAK = 0x00000040;
+        public const int MF_SEPARATOR = 0x00000800;
+
+        public const int MF_BYCOMMAND = 0x00000000;
+        public const int MF_BYPOSITION = 0x00000400;
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern uint TrackPopupMenu(IntPtr hMenu, uint uFlags, int x, int y, int nReserved, IntPtr hWnd, IntPtr prcRect);
+
+        public const int TPM_LEFTBUTTON = 0x0000;
+        public const int TPM_RIGHTBUTTON = 0x0002;
+        public const int TPM_LEFTALIGN = 0x0000;
+        public const int TPM_CENTERALIGN = 0x0004;
+        public const int TPM_RIGHTALIGN = 0x0008;
+        public const int TPM_TOPALIGN = 0x0000;
+        public const int TPM_VCENTERALIGN = 0x0010;
+        public const int TPM_BOTTOMALIGN = 0x0020;
+        public const int TPM_HORIZONTAL = 0x0000;     /* Horz alignment matters more */
+        public const int TPM_VERTICAL = 0x0040;     /* Vert alignment matters more */
+        public const int TPM_NONOTIFY = 0x0080;     /* Don't send any notification msgs */
+        public const int TPM_RETURNCMD = 0x0100;
+        public const int TPM_RECURSE = 0x0001;
+        public const int TPM_HORPOSANIMATION = 0x0400;
+        public const int TPM_HORNEGANIMATION = 0x0800;
+        public const int TPM_VERPOSANIMATION = 0x1000;
+        public const int TPM_VERNEGANIMATION = 0x2000;
+        public const int TPM_NOANIMATION = 0x4000;
+        public const int TPM_LAYOUTRTL = 0x8000;
+        public const int TPM_WORKAREA = 0x10000;
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern int PostMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode, EntryPoint = "GetProcAddress")]
+        public static extern IntPtr GetProcAddressByOrdinal(IntPtr hModule, int lpProcName);
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+        public static extern IntPtr LoadLibrary(string lpLibFileName);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr LoadImage(IntPtr hInst, string lpszName, UInt32 uType, int cxDesired, int cyDesired, UInt32 fuLoad);
+
+        public const int IMAGE_BITMAP = 0;
+        public const int IMAGE_ICON = 1;
+        public const int IMAGE_CURSOR = 2;
+        public const int IMAGE_ENHMETAFILE = 3;
+
+        public const int LR_DEFAULTCOLOR = 0x00000000;
+        public const int LR_MONOCHROME = 0x00000001;
+        public const int LR_COLOR = 0x00000002;
+        public const int LR_COPYRETURNORG = 0x00000004;
+        public const int LR_COPYDELETEORG = 0x00000008;
+        public const int LR_LOADFROMFILE = 0x00000010;
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool DestroyIcon(IntPtr hIcon);
+
+        private IntPtr hWndMain = IntPtr.Zero;
+        private IntPtr m_hIcon = IntPtr.Zero;
+        private IntPtr m_hBalloonIcon = IntPtr.Zero;
+
+        private SUBCLASSPROC SubClassDelegate;
+
+        private void HideInTray()
+        {
+            // 弹出系统通知 TrayMessage(hWndMain, null, m_hIcon, m_hBalloonIcon, NIM.MODIFY, NIIF.USER | NIIF.LARGE_ICON, "按下快捷键或点击图标可以唤出窗口", "燧石 已隐藏至系统托盘", 0);
+            this.Hide();
+        }
+
+        private int WindowSubClass(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam, IntPtr uIdSubclass, uint dwRefData)
+        {
+            switch (uMsg)
+            {
+                case WM_TRAYMOUSEMESSAGE:
+                    {
+                        switch (LOWORD((int)lParam))
+                        {
+                            case WM_LBUTTONUP:
+                                this.Restore();
+                                this.BringToFront();
+                                break;
+                            case WM_CONTEXTMENU:
+                            case WM_RBUTTONUP:
+                                {
+                                    Windows.Graphics.PointInt32 ptCursor;
+                                    GetCursorPos(out ptCursor);
+
+                                    IntPtr hMenu = CreatePopupMenu();
+                                    int i = 1;
+                                    AppendMenu(hMenu, MF_STRING, (IntPtr)i, "显示窗口");
+                                    i += 1;
+                                    // 添加分割线 AppendMenu(hMenu, MF_SEPARATOR, (IntPtr)i, null); i += 1;
+                                    AppendMenu(hMenu, MF_STRING, (IntPtr)i, "退出");
+                                    SetForegroundWindow(hWnd);
+                                    uint nCmd = TrackPopupMenu(hMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD, ptCursor.X, ptCursor.Y, 0, hWnd, IntPtr.Zero);
+                                    PostMessage(hWnd, 0x0000, IntPtr.Zero, IntPtr.Zero);
+                                    if (nCmd != 0)
+                                    {
+                                        if (nCmd == i)
+                                        {
+                                            this.Close();
+                                        }
+                                        else if (nCmd == 1)
+                                        {
+                                            this.Restore();
+                                            this.BringToFront();
+                                        }
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                    break;
+            }
+            return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+        }
+
+        private void CleanTray()
+        {
+            // 移除托盘图标
+            TrayMessage(hWndMain, null, IntPtr.Zero, IntPtr.Zero, NIM.DELETE, NIIF.NONE, null, null, 0);
+
+            if (m_hIcon != IntPtr.Zero)
+                DestroyIcon(m_hIcon);
+            if (m_hBalloonIcon != IntPtr.Zero)
+                DestroyIcon(m_hBalloonIcon);
+        }
+
+        #endregion
+
     }
 }
