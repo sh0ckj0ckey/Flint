1ba2f18026600c44a3b61e58976e0999721eabfb
姚一鸣 <yaoyiming123@live.com>
2025-04-08 11:08:15 +0800

    m

--- Flint3.Interop/KeyboardHook.cpp
+++ Flint3.Interop/KeyboardHook.cpp
@@ -10,62 +10,52 @@ using namespace System;
 using namespace System::Diagnostics;
 
 KeyboardHook::KeyboardHook(
-    KeyboardEventCallback^ keyboardEventCallback,
-    IsActiveCallback^ isActiveCallback,
-    FilterKeyboardEvent^ filterKeyboardEvent)
+	KeyboardEventCallback^ keyboardEventCallback,
+	IsActiveCallback^ isActiveCallback,
+	FilterKeyboardEvent^ filterKeyboardEvent)
 {
-    this->keyboardEventCallback = keyboardEventCallback;
-    this->isActiveCallback = isActiveCallback;
-    this->filterKeyboardEvent = filterKeyboardEvent;
+	this->keyboardEventCallback = keyboardEventCallback;
+	this->isActiveCallback = isActiveCallback;
+	this->filterKeyboardEvent = filterKeyboardEvent;
 }
 
 KeyboardHook::~KeyboardHook()
 {
-    // Unregister low level hook procedure
-    UnhookWindowsHookEx(hookHandle);
+	// Unregister low level hook procedure
+	UnhookWindowsHookEx(hookHandle);
 }
 
 void KeyboardHook::Start()
 {
-    hookProc = gcnew HookProcDelegate(this, &KeyboardHook::HookProc);
-#if defined(DISABLE_LOWLEVEL_HOOKS_WHEN_DEBUGGED)
-    const bool hookDisabled = IsDebuggerPresent();
-#else
-    const bool hookDisabled = false;
-#endif
-    if (!hookDisabled)
-    {
-        // register low level hook procedure
-        hookHandle = SetWindowsHookEx(
-            WH_KEYBOARD_LL,
-            (HOOKPROC)(void*)Marshal::GetFunctionPointerForDelegate(hookProc),
-            0,
-            0);
-        if (hookHandle == nullptr)
-        {
-            //DWORD errorCode = GetLastError();
-            //show_last_error_message(L"SetWindowsHookEx", errorCode, L"Flint3 - Interop");
-        }
-    }
+	hookProc = gcnew HookProcDelegate(this, &KeyboardHook::HookProc);
+
+	// register low level hook procedure
+	hookHandle = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)(void*)Marshal::GetFunctionPointerForDelegate(hookProc), 0, 0);
+	if (hookHandle == nullptr)
+	{
+		DWORD errorCode = GetLastError();
+		//show_last_error_message(L"SetWindowsHookEx", errorCode, L"Flint3 - Interop");
+	}
 }
 
 LRESULT __clrcall KeyboardHook::HookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    if (nCode == HC_ACTION && isActiveCallback->Invoke())
-    {
-        KeyboardEvent^ ev = gcnew KeyboardEvent();
-        ev->message = wParam;
-        ev->key = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam)->vkCode;
-        ev->dwExtraInfo = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam)->dwExtraInfo;
+	if (nCode == HC_ACTION && isActiveCallback->Invoke())
+	{
+		KeyboardEvent^ ev = gcnew KeyboardEvent();
+		ev->message = wParam;
+		ev->key = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam)->vkCode;
+		ev->dwExtraInfo = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam)->dwExtraInfo;
 
-        // Ignore the keyboard hook if the FilterkeyboardEvent returns false.
-        if ((filterKeyboardEvent != nullptr && !filterKeyboardEvent->Invoke(ev)))
-        {
-            return CallNextHookEx(hookHandle, nCode, wParam, lParam);
-        }
+		// Ignore the keyboard hook if the FilterkeyboardEvent returns false.
+		if ((filterKeyboardEvent != nullptr && !filterKeyboardEvent->Invoke(ev)))
+		{
+			return CallNextHookEx(hookHandle, nCode, wParam, lParam);
+		}
 
-        keyboardEventCallback->Invoke(ev);
-        return 1;
-    }
-    return CallNextHookEx(hookHandle, nCode, wParam, lParam);
+		keyboardEventCallback->Invoke(ev);
+		return 1;
+	}
+
+	return CallNextHookEx(hookHandle, nCode, wParam, lParam);
 }
